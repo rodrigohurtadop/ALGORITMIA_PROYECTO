a62f38b01ddb33cfff34e108a7bb19c3d8f7804d
rodrigohurtadop <i202405517@cibertec.edu.pe>
2024-10-06 19:57:36 -0500

    Cerrar interfaz
    es DISPOSE en lugar de CLOSE

--- PROYECTO/src/guis/Mantenimiento_Clientes.java
+++ PROYECTO/src/guis/Mantenimiento_Clientes.java
@@ -53,7 +53,7 @@ public class Mantenimiento_Clientes extends JFrame {
 	 * Create the frame.
 	 */
 	public Mantenimiento_Clientes() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 468, 653);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
