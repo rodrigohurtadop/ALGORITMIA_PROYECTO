93e40523a7e9e25c5464d3266f2c66c758a88a18
rodrigohurtadop <i202405517@cibertec.edu.pe>
2024-10-12 21:50:15 -0500

    Abrir una ventana a la vez

--- PROYECTO/src/guis/Main.java
+++ PROYECTO/src/guis/Main.java
@@ -1,7 +1,6 @@
 package guis;
 
 import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -10,100 +9,128 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 
 public class Main extends JFrame {
 
-	private static final long serialVersionUID = 1L;
-	private JPanel contentPane;
+    private static final long serialVersionUID = 1L;
+    private JPanel contentPane;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Main frame = new Main();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+    private JMenuItem mntmClientes;
+    private JMenuItem mntmProductos;
+    private JMenuItem mntmVentas;
+    private JMenuItem mntmStock;
+    private JMenuItem mntmListado;
 
-	/**
-	 * Create the frame.
-	 */
-	public Main() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 688, 445);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Main frame = new Main();
+                    frame.setVisible(true);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JMenuBar menuBar = new JMenuBar();
-		menuBar.setBounds(0, 0, 672, 22);
-		contentPane.add(menuBar);
-		
-		JMenu mnNewMenu = new JMenu("Mantenimiento");
-		menuBar.add(mnNewMenu);
-		
-		JMenuItem mntmNewMenuItem = new JMenuItem("Clientes");
-		mntmNewMenuItem.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Mantenimiento_Clientes Mantenimiento = new Mantenimiento_Clientes();
-				Mantenimiento.setVisible(true);
-			}
-		});
-		mnNewMenu.add(mntmNewMenuItem);
-		
-		JMenuItem mntmNewMenuItem_1 = new JMenuItem("Productos");
-		mntmNewMenuItem_1.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Mantenimiento_Productos Producto = new Mantenimiento_Productos();
-				Producto.setVisible(true);				
-			}
-		});
-		mnNewMenu.add(mntmNewMenuItem_1);
-		
-		JMenu mnNewMenu_1 = new JMenu("Ventas");
-		menuBar.add(mnNewMenu_1);
-		
-		JMenuItem mntmNewMenuItem_2 = new JMenuItem("Ventas");
-		mntmNewMenuItem_2.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Ventas_Ventas Ventas = new Ventas_Ventas();
-				Ventas.setVisible(true);
-			}
-		});
-		mnNewMenu_1.add(mntmNewMenuItem_2);
-		
-		JMenu mnNewMenu_2 = new JMenu("Almacen");
-		menuBar.add(mnNewMenu_2);
-		
-		JMenuItem mntmNewMenuItem_3 = new JMenuItem("Stock");
-		mntmNewMenuItem_3.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Almacen_Stock Stock = new Almacen_Stock();
-				Stock.setVisible(true);
-			}
-		});
-		mnNewMenu_2.add(mntmNewMenuItem_3);
-		
-		JMenu mnNewMenu_3 = new JMenu("Reportes");
-		menuBar.add(mnNewMenu_3);
-		
-		JMenuItem mntmNewMenuItem_4 = new JMenuItem("Listado");
-		mntmNewMenuItem_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				Reportes_Listado Listado = new Reportes_Listado();
-				Listado.setVisible(true);
-			}
-		});
-		mnNewMenu_3.add(mntmNewMenuItem_4);
-	}
+    /**
+     * Create the frame.
+     */
+    public Main() {
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBounds(100, 100, 688, 445);
+        contentPane = new JPanel();
+        contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+        setContentPane(contentPane);
+        contentPane.setLayout(null);
+
+        JMenuBar menuBar = new JMenuBar();
+        menuBar.setBounds(0, 0, 672, 22);
+        contentPane.add(menuBar);
+
+        JMenu mnMantenimiento = new JMenu("Mantenimiento");
+        menuBar.add(mnMantenimiento);
+
+        mntmClientes = new JMenuItem("Clientes");
+        mntmClientes.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirVentana(new Mantenimiento_Clientes());
+            }
+        });
+        mnMantenimiento.add(mntmClientes);
+
+        mntmProductos = new JMenuItem("Productos");
+        mntmProductos.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirVentana(new Mantenimiento_Productos());
+            }
+        });
+        mnMantenimiento.add(mntmProductos);
+
+        JMenu mnVentas = new JMenu("Ventas");
+        menuBar.add(mnVentas);
+
+        mntmVentas = new JMenuItem("Ventas");
+        mntmVentas.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirVentana(new Ventas_Ventas());
+            }
+        });
+        mnVentas.add(mntmVentas);
+
+        JMenu mnAlmacen = new JMenu("Almacen");
+        menuBar.add(mnAlmacen);
+
+        mntmStock = new JMenuItem("Stock");
+        mntmStock.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirVentana(new Almacen_Stock());
+            }
+        });
+        mnAlmacen.add(mntmStock);
+
+        JMenu mnReportes = new JMenu("Reportes");
+        menuBar.add(mnReportes);
+
+        mntmListado = new JMenuItem("Listado");
+        mntmListado.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                abrirVentana(new Reportes_Listado());
+            }
+        });
+        mnReportes.add(mntmListado);
+    }
+
+    /**
+     * Método para abrir una ventana secundaria y desactivar los menús.
+     */
+    private void abrirVentana(JFrame ventana) {
+        setMenuItemsEnabled(false);
+
+        ventana.setVisible(true);
+
+        ventana.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosed(WindowEvent e) {
+                setMenuItemsEnabled(true);
+            }
+        });
+    }
+
+    /**
+     * Método para habilitar o deshabilitar los JMenuItem.
+     */
+    private void setMenuItemsEnabled(boolean enabled) {
+        mntmClientes.setEnabled(enabled);
+        mntmProductos.setEnabled(enabled);
+        mntmVentas.setEnabled(enabled);
+        mntmStock.setEnabled(enabled);
+        mntmListado.setEnabled(enabled);
+    }
 }
-
